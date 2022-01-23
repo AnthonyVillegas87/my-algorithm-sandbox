a6a177ff19cd728b150314c965689e520d64eacc
Anthony Villegas <villegasanthony87@yahoo.com>
2022-01-22 22:45:10 -0600

    feat: vowelCount algo completed

--- 03-Algorithms/03-vowel-count/Unsolved/vowel-count.js
+++ 03-Algorithms/03-vowel-count/Unsolved/vowel-count.js
@@ -1,3 +1,16 @@
-// Write code to return the the number of vowels in `str`
+// Write code to return the number of vowels in `str`
 
-var vowelCount = function(str) {};
+let vowelCount = function(str) {
+    let vowels = ["a", "e", "i", "o", "u"];
+    let count = 0;
+    let newString = str.toLowerCase().split("");
+    newString.filter((str) => {
+        count += vowels.includes(str)
+
+
+
+    })
+
+    return count
+
+};
