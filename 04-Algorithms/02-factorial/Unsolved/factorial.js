ffc3ec2783f4d292a3e49b56480413fbb19f2e85
Anthony Villegas <villegasanthony87@yahoo.com>
2022-01-23 22:21:00 -0600

    feat: factorial  completed

--- 04-Algorithms/02-factorial/Unsolved/factorial.js
+++ 04-Algorithms/02-factorial/Unsolved/factorial.js
@@ -1,3 +1,6 @@
 // Write code to create a function that returns the factorial of `num`
 
-var factorial = function(num) {};
+let factorial = function(num) {
+    if (num === 0) return 1
+    return num * factorial(num - 1)
+};
