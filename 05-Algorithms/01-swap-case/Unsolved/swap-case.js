41494923c31fe92b4941638097e9df2a03d370d1
Anthony Villegas <villegasanthony87@yahoo.com>
2022-01-26 20:37:14 -0600

    feat: swapCase function complete

--- 05-Algorithms/01-swap-case/Unsolved/swap-case.js
+++ 05-Algorithms/01-swap-case/Unsolved/swap-case.js
@@ -1,3 +1,15 @@
 // Write code to create a function takes a string and returns the string with all of the letter cases swapped
 
-var swapCase = function(str) {};
+let swapCase = function(str) {
+    let arr = [];
+    str.split("").forEach(  el => {
+
+       if(el.toUpperCase() === el) {
+           arr.push(el.toLowerCase());
+       } else {
+           arr.push(el.toUpperCase());
+       }
+
+    });
+    return arr.join('');
+};
