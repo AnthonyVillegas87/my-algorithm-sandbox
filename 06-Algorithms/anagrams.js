f1046509a96df3375fafa6c3a0a8433e8192382a
Anthony Villegas <villegasanthony87@yahoo.com>
2022-01-27 11:13:57 -0600

    feat: two pointers pattern

--- 06-Algorithms/anagrams.js
+++ 06-Algorithms/anagrams.js
@@ -1,48 +1,52 @@
 function validAnagram(str1, str2) {
-    // let counterOne = {};
-    // let counterTwo = {};
-    //
-    // //iterate through the first str1
-    // for(let val in  str1) {
-    //     counterOne[val] = (counterOne[val] || 0) + 1
-    //
-    // }
-    //
-    // //iterate through the second str2
-    // for(let val in str2) {
-    //     counterTwo[val] = (counterTwo[val] || 0) + 1
-    //
-    // }
-    //
-    // //iterate through the first counter1 & compare to the second
-    // for(let key in counterOne) {
-    //
-    //     if(counterTwo[key] !== counterOne[key]) {
-    //         return false
-    //     }
-    // }
-    //
-    // return true
+    if(str1.length !== str2.length) return false
 
+    let counterOne = {};
+    let counterTwo = {};
 
-    const lookup = {};
+    //iterate through the first str1
+    for(let val in  str1) {
+        counterOne[val] = (counterOne[val] || 0) + 1
 
-    for(let i = 0; i < str1.length; i++) {
-        let letter = str1[i];
-        //if letter exists, increment, otherwise set to 1
-        lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1;
     }
 
-    for(let i = 0; i < str2.length; i++) {
-        let letter = str2[i];
-        if (!lookup[letter]) {
+    //iterate through the second str2
+    for(let val in str2) {
+        counterTwo[val] = (counterTwo[val] || 0) + 1
+
+    }
+
+    //iterate through the first counter1 & compare to the second
+    for(let key in counterOne) {
+         if(!key in counterTwo) {
             return false
-        } else {
-            lookup[letter] -= 1
+         }
+        if(counterTwo[key] !== counterOne[key]) {
+            return false
         }
     }
 
     return true
+
+
+    // const lookup = {};
+    //
+    // for(let i = 0; i < str1.length; i++) {
+    //     let letter = str1[i];
+    //     //if letter exists, increment, otherwise set to 1
+    //     lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1;
+    // }
+    //
+    // for(let i = 0; i < str2.length; i++) {
+    //     let letter = str2[i];
+    //     if (!lookup[letter]) {
+    //         return false
+    //     } else {
+    //         lookup[letter] -= 1
+    //     }
+    // }
+    //
+    // return true
 
 
 
