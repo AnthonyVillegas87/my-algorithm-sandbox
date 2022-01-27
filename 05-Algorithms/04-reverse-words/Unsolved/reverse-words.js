40f622fcc53fdd05f6e860f163ff6e9a5ea8a7d9
Anthony Villegas <villegasanthony87@yahoo.com>
2022-01-26 22:39:54 -0600

    feat: reverse words func

--- 05-Algorithms/04-reverse-words/Unsolved/reverse-words.js
+++ 05-Algorithms/04-reverse-words/Unsolved/reverse-words.js
@@ -1,3 +1,7 @@
 // Write code to create a function that accepts a string (`str`) and reverses the order of the words in the string
 
-var reverseWords = function(str) {};
+let reverseWords = function(str) {
+
+   return str.split(' ').reverse().join(' ')
+
+};
