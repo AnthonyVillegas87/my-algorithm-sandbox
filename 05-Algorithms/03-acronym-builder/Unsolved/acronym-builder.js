01e684e0e2a41f7af43a9c2d54a082cf97a9c14e
Anthony Villegas <villegasanthony87@yahoo.com>
2022-01-26 22:27:41 -0600

    feat: acronym builder func finished

--- 05-Algorithms/03-acronym-builder/Unsolved/acronym-builder.js
+++ 05-Algorithms/03-acronym-builder/Unsolved/acronym-builder.js
@@ -1,3 +1,14 @@
 // Write code to create a function that accepts a string and returns an acronym for the given string
 
-var acronymBuilder = function(str) {};
+let acronymBuilder = function(str) {
+    // Assign split str to var to separate each word in the sentence
+    let word = str.split(' ');
+
+    //iterate through array and find the first char of each word
+ return word.reduce((prev, curr) => {
+
+     return prev + curr.charAt().toUpperCase()
+
+  }, "")
+
+};
