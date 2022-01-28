9d74148a23141e978a3880aa14646ec15a6e9005
Anthony Villegas <villegasanthony87@yahoo.com>
2022-01-27 19:16:13 -0600

    feat: completed avgPair w/ multiple pointers algo

--- 06-Algorithms/multiple-pointers.js
+++ 06-Algorithms/multiple-pointers.js
@@ -41,3 +41,42 @@ console.log(countUniqueValues([1,1,1,1,1,2]))  //2
 console.log(countUniqueValues([1,2,3,4,4,4,7,7,12,12,13])) //7
 console.log(countUniqueValues([])) // 0
 console.log(countUniqueValues([-2, -1, -1, 0, 1])) //4
+
+
+/*
+write a function called avgPair. Give the sorted array of integers and a target average,
+determine if there is a pair of values in the array where the average of the pair equals
+the target average. There may be more than one pair that matches the average target
+ */
+
+function avgPair(arr, avg) {
+    //edge case for empty array
+    if(arr.length === 0) return false
+
+    let j = 0;
+    let i;
+
+    while(j < arr.length && arr[j] < 0) {
+        if( j / arr.length !== avg) {
+            return false
+        }
+        j++
+        i = j;
+    }
+
+
+
+    return true
+}
+
+console.log(avgPair([1, 2, 3], 2.5))  //true
+console.log(avgPair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8)) //true
+console.log(avgPair([-1, 0, 3, 4, 5, 6], 4.1)) //false
+console.log(avgPair([], 4)) //false
+
+
+
+
+
+
+
