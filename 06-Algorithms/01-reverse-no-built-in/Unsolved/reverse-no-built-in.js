613f07b2feefa0a37f9c510a06a0205ea2d3ec9b
Anthony Villegas <villegasanthony87@yahoo.com>
2022-01-30 20:13:31 -0600

    feat: reverse function algo complete

--- 06-Algorithms/01-reverse-no-built-in/Unsolved/reverse-no-built-in.js
+++ 06-Algorithms/01-reverse-no-built-in/Unsolved/reverse-no-built-in.js
@@ -1,6 +1,13 @@
 // Write code to return a new string with all of the characters in `str` reversed.
 // You may NOT use the built-in reverse method
 
-var reverse = function(str) {
+let reverse = function(str) {
+    let array = str.split('');
+    let result = [];
 
+    for(let i = array.length - 1; i >= 0; i--) {
+        result.push(array[i])
+    }
+
+    return result.join('');
 };
