3874e169c693d3b407e7c5b67a881668653c1a27
Anthony Villegas <villegasanthony87@yahoo.com>
2022-02-02 13:07:11 -0600

    feat: singly linked list intro complete

--- 06-Algorithms/02-reverse-in-place/Unsolved/reverse-in-place.js
+++ 06-Algorithms/02-reverse-in-place/Unsolved/reverse-in-place.js
@@ -1,12 +1,8 @@
 // Write code to return a new string with all of the characters in `str` reversed.
 // You may NOT use the built-in reverse method
 
-function swap(arr, i, j) {
-    let element = arr[i];
-    arr[i] = arr[j];
-    arr[j] = element;
-}
+
 
 let reverseInPlace = function(arr) {
-
+    console.log(arr)
 };
--- data-structures/singly-linked-list.js
+++ data-structures/singly-linked-list.js
@@ -124,6 +124,23 @@ class SinglyLinkedList {
         return removed;
 
     }
+    //reversing the linked list in place
+    reverse() {
+       let node = this.head;
+       this.head = this.tail;
+       this.tail = node;
+
+       let next;
+       let prev = null;
+
+       for(let i = 0; i < this.length; i++) {
+           next = node.next;
+           node.next = prev;
+           prev = node;
+           node = next;
+       }
+       return this
+    }
 }
 
 let list = new SinglyLinkedList()
