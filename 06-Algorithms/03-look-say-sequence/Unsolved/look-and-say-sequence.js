58e83059eb429d770d9ea6ce2149becbbadded60
Anthony Villegas <villegasanthony87@yahoo.com>
2022-02-20 07:04:47 -0600

    feat: look and say sequence

--- 06-Algorithms/03-look-say-sequence/Unsolved/look-and-say-sequence.js
+++ 06-Algorithms/03-look-say-sequence/Unsolved/look-and-say-sequence.js
@@ -1,5 +1,19 @@
 // Write code to create a function that accepts a number (`n`) and returns a new number that satisfies the look-and-say-sequence
 
-var lookAndSay = function(n) {
+let lookAndSay = function(n) {
+    n = n + '*';
+    let output = "";
+    let count = 0;
 
+    for(let i = 1; i < n.length; i++) {
+        if(n.charAt(i) !== n.charAt(i - 1)) {
+          output += (i - count) + n.substring(count, count + 1);
+          count = i;
+        }
+    }
+
+    console.log(output)
+   return parseInt(output);
 };
+
+
